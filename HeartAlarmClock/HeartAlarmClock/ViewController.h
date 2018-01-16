3f174c4f693e877d8c6910618ff926654058fb5a
lice <979900351@qq.com>
2018-01-16 16:00:07 +0800

    初始化项目

---
+++ HeartAlarmClock/HeartAlarmClock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeartAlarmClock
+//
+//  Created by xingjian on 2018/1/16.
+//  Copyright © 2018年 xingjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HeartAlarmClock/HeartAlarmClockTests/HeartAlarmClockTests.m
@@ -0,0 +1,39 @@
+//
+//  HeartAlarmClockTests.m
+//  HeartAlarmClockTests
+//
+//  Created by xingjian on 2018/1/16.
+//  Copyright © 2018年 xingjian. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HeartAlarmClockTests : XCTestCase
+
+@end
+
+@implementation HeartAlarmClockTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
